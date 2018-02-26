dad85689834bfd74cdf3093b7a2a1f914b122abd
baassirk1 <35268798+baassirk1@users.noreply.github.com>
2018-02-25 22:13:15 -0500

    Update ProjectAnalyze.sh

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -85,18 +85,3 @@ echo "
 
 
 Very well. Report to General D'Alves now. I'll see you on the battlefield. Manadlore, out."
-
-#Hey! I'm Amonkira, and I'll be your assistant today. All you have to do is check follow the instructions given in the #PROJECTANALYZE.SH file, and you should be good.
-
-#If you'd like to know the answers... (for reference later) please check below.
-
-#1) great / horrible
-#2) curtis
-#3) y
-#4) 1XA3
-
-
-#In reference to  https://github.com/mudarthc/CS1XA3/blob/master/Assign1/ProjectAnalyze.sh for inspiring the idea of an interface
-
-#End Script
-
